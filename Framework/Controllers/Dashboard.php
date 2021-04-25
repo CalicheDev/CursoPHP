514cd248679069e1792213fbf8bb9c5af1ce6dc1
charlycoin <cabejarano21@gmail.com>
2021-04-25 18:36:02 -0500

    add nuevos archivos

--- Framework/Controllers/Dashboard.php
+++ Framework/Controllers/Dashboard.php
@@ -12,7 +12,6 @@
 			$data['page_tag'] = "Dashboard - Tienda Virtual";
 			$data['page_title'] = "Dashboard - Tienda Virtual";
 			$data['page_name'] = "dashboard";
-			//$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
 			$this->views->getView($this,"dashboard",$data);
 		}
 
