ed94a501b2cef238c450857dd70cf3b5ac1e0668
charlycoin <cabejarano21@gmail.com>
2021-05-02 16:37:53 -0500

    upd nuevo templeate

--- SoftwebTickets/Controllers/Dashboard.php
+++ SoftwebTickets/Controllers/Dashboard.php
@@ -4,6 +4,14 @@
 		public function __construct()
 		{
 			parent::__construct();
+			session_start();
+			session_regenerate_id(true);
+			if(empty($_SESSION['login']))
+			{
+				header('Location: '.base_url().'/login');
+				die();
+			}
+			getPermisos(1);
 		}
 
 		public function dashboard()
@@ -12,7 +20,7 @@
 			$data['page_tag'] = "Dashboard - Tienda Virtual";
 			$data['page_title'] = "Dashboard - Tienda Virtual";
 			$data['page_name'] = "dashboard";
-			//$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
+			$data['page_functions_js'] = "functions_dashboard.js";
 			$this->views->getView($this,"dashboard",$data);
 		}
 
